0bf7387f49560302b5fba06e2cc183359081f42c
Jacob <jakub.zomerfeld@gmail.com>
2019-05-25 19:21:08 +0200

    Fix for js day order, added animation to wallpaper and text

--- todayDay.js
+++ todayDay.js
@@ -1,4 +1,4 @@
-function myDay() {
+function todayDay() {
 
 //create a new date obj with current date+time
 let today = new Date();
@@ -7,16 +7,15 @@ let today = new Date();
 let day = today.getDay();
 
 //array with a list of weekday (names)
-let daylist = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    let daylist = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 // testing day display
 // console.log (daylist[day]);
 
-
 // define text print layout
-let myDay = "Today is " + daylist[day]
+let todayDay = "Today is " + daylist[day]
 
-document.getElementById('showDay').innerHTML = myDay;
+document.getElementById('showDay').innerHTML = todayDay;
 
 }
-myDay()
+todayDay()
